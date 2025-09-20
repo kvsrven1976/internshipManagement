dab84f7e646a146cbdc790c9a548740b744e47fc
kvsrven76 <kvsrven76@gmail.com>
2025-09-20 11:38:46 +0530

    InternshipManagement_Documentation

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c64729ad3d147e39ccbd2d320a6f7b60e1e9fb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c5a3541f76d081864257344ef17fb946781399e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
