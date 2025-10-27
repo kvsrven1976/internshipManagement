1ea360eefc5e6e41c24889cd8727766973209eff
kvsrven76 <kvsrven76@gmail.com>
2025-10-27 07:16:01 +0530

    backup

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c5a3541f76d081864257344ef17fb946781399e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8054a610d0305d4bd15d7977359034445708585a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
